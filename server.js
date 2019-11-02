8213b4b92c182a6e9d2fb5cc6c1246d32729a588
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-02 11:02:02 -0600

    completed controller functions for first two endpoints

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const userCtrl = require('./usersCtrl')
 const SERVER_PORT = 3000;
 
 const app = express();
 
 app.use(express.json())
+
+app.get('/api/user', userCtrl.users)
+app.get('/api/user/:userId', userCtrl.user)
+app.get('/api/admin', userCtrl.admin)
+app.get('/api/nonAdmin', userCtrl.nonAdmin)
+app.get('/api/type/:userType', userCtrl.userType)
+app.put('/api/user/:userId', userCtrl.userUpdate)
+app.post('/api/user', userCtrl.newUser)
+app.delete('/api/user/:userId', userCtrl.deleteUser)
+
+
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
 
--- usersCtrl.js
+++ usersCtrl.js
@@ -1,1 +1,108 @@
 let userData = require("./userData.json")
+
+module.exports = {
+    // GET /api/user Respond with entire usersarray, with stat 200
+    //  Possible Queries
+    //  - "age" Return all users who have an age less than this age
+    // - "email" Return all users whose email matches.
+    // - "favorites" Return all users who have this favorite in their array of favorites.
+    async users(req, res) {
+        const {age, email, favorites} = req.query
+        let filteredArr = [];
+
+        if (age) {
+            filteredArr = userData.filter((element) => {
+                if (element.age < age) {
+                    return true
+                } else {
+                    return false
+                }
+            })
+
+            res.status(200).send(filteredArr)
+        }
+
+        if (email) {
+            filteredArr = userData.filter((element) => {
+                if (element.email === email) {
+                    return true
+                } else {
+                    return false
+                }
+            })
+
+            res.status(200).send(filteredArr)
+        }
+
+        if (favorites) {
+            filteredArr = userData.filter((element) => {
+                for (let i = 0; i < element.favorites.length; i++) {
+                    if (element.favorites[i] === favorites) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            })
+
+            res.status(200).send(filteredArr)
+        }
+
+        res.status(200).send(userData)
+
+    },
+    // GET /api/user/ + userId
+    // userId as a param string, if user found respond with status 200 and send that user's object/
+    // Otherwise, 404
+    async user(req, res) {
+        const {userId} = req.params;
+        // I could take advantage of the fact the ids are in array order,
+        // but in this case I choose to pretend that it wasn't.
+        // This could be solved as returning userData[parseInt(userId)] since they are in order.
+        const filteredArr = userData.filter((element) => {
+            if (element.id === parseInt(userId)) {
+                return true
+            } else {
+                return false
+            }
+        })
+
+        if (filteredArr.length > 0) {
+            res.status(200).send(filteredArr)
+        } else {
+            res.sendStatus(404)
+        }
+    },
+    // GET /api/admin
+    // Respond 200, with array of all users who are admins
+    async admin(req, res) {
+
+    },
+    // GET /api/nonadmin
+    // Respond 200, with array of all non-admin users.
+    async nonAdmin(req, res) {
+
+    },
+    // GET /api/type/ + userType
+    async userType(req, res) {
+
+    },
+    // PUT /api/user/ + userId
+    // Recieve a users info in request body. Update user with matching ID
+    async userUpdate(req, res) {
+
+    },
+    // POST /api/user
+    // Users info in request body to be added to userData array.
+    // Add ID before pushing it to userData
+    async newUser(req, res) {
+
+    },
+    // DELETE /api/user/ + userId
+    // Remove user based on passed in userID param
+    // Return status 200 and the array of user objects after the correct user object has been removed.
+    async deleteUser(req, res) {
+
+    }
+
+}
